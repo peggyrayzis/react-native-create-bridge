a0070cd645eefad16aac866d8afd7ed1e77db00c
Kurtis Kemple <kurtiskemple@gmail.com>
2017-01-11 16:28:53 -0500

    add obj-c templates, simplify android templates

---
+++ templates/ios-objc/Template.h
@@ -0,0 +1,10 @@
+//
+//  {{template}}.h
+//
+//  Created by react-native-create-bridge on {{date}}.
+
+#import "RCTBridgeModule.h"
+
+@interface {{template}} : NSObject <RCTBridgeModule>
+  // Define class properties here
+@end
---
+++ templates/ios-objc/TemplateManager.m
@@ -0,0 +1,45 @@
+//
+//  {{template}}.m
+//
+//  Created by react-native-create-bridge on {{date}}.
+
+#import "RCTBridge.h"
+#import "RCTEventDispatcher.h"
+
+@implementation {{template}}
+@synthesize bridge = _bridge;
+
+// Export a native module
+// https://facebook.github.io/react-native/docs/native-modules-ios.html
+RCT_EXPORT_MODULE();
+
+- (NSDictionary *)constantsToExport
+{
+  return @{
+           @"EXAMPLE": @"example"
+         };
+}
+
+// Return the native view that represents your React component
+- (UIView *)view
+{
+  return [[UIView alloc] init];
+}
+
+// Export methods to a native module
+// https://facebook.github.io/react-native/docs/native-modules-ios.html
+RCT_EXPORT_METHOD(exampleMethod)
+{
+  [self.emitMessageToRN:@"EXAMPLE_EVENT" :nil]
+}
+
+#pragma mark - Private methods
+
+// Implement methods that you want to export to the native module
+- (void) emitMessageToRN: (NSString *)eventName :(NSDictionary *)params {
+  // The bridge eventDispatcher is used to send events from native to JS env
+  // No documentation yet on DeviceEventEmitter: https://github.com/facebook/react-native/issues/2819
+  [self.bridge.eventDispatcher sendAppEventWithName: eventName body: params];
+}
+
+@end
