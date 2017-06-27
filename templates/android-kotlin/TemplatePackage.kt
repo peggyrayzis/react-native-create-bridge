4e28619e7d906dc973a83d781c8c647b224fe9e0
Peggy Rayzis <peggyrayzis@gmail.com>
2017-06-27 10:54:18 -0400

    style(kotlin): clean up semicolons

--- templates/android-kotlin/TemplatePackage.kt
+++ templates/android-kotlin/TemplatePackage.kt
@@ -1,14 +1,14 @@
 //  Created by react-native-create-bridge
 
-package com.{{app}}.{{packageName}};
+package com.{{app}}.{{packageName}}
 
-import com.facebook.react.ReactPackage;
-import com.facebook.react.bridge.JavaScriptModule;
-import com.facebook.react.bridge.NativeModule;
-import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.react.uimanager.ViewManager;
+import com.facebook.react.ReactPackage
+import com.facebook.react.bridge.JavaScriptModule
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.uimanager.ViewManager
 
-import java.util.Arrays;
+import java.util.Arrays
 
 class {{template}}Package : ReactPackage {
     
