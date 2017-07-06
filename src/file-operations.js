3b352f0bb755d494fc83694dbd98606aec699487
Peggy Rayzis <peggyrayzis@gmail.com>
2017-07-06 00:23:26 -0400

    feat(file ops, init): finished logic to split out bridges

--- src/file-operations.js
+++ src/file-operations.js
@@ -1,10 +1,19 @@
-import path from 'path';
-import fs from 'mz/fs';
+import path from "path";
+import fs from "mz/fs";
 
-export const pkg = require(path.join(process.cwd(), 'package.json'));
+export const pkg = require(path.join(process.cwd(), "package.json"));
+
+export async function getFileNames(dirPath) {
+  const fileNames = await fs
+    .readdir(dirPath)
+    .catch(e => console.error("[getFileNames] ", e));
+  return fileNames;
+}
 
 export async function readFile(file, readDirPath) {
-  const fileData = await fs.readFile(path.join(readDirPath, file), 'utf-8');
+  const fileData = await fs
+    .readFile(path.join(readDirPath, file), "utf-8")
+    .catch(e => console.error("[readFile] ", e));
   return fileData;
 }
 
@@ -12,7 +21,7 @@ export function parseFile(
   fileData,
   templateName,
   packageName = null,
-  app = null,
+  app = null
 ) {
   return fileData
     .replace(/{{template}}/g, templateName)
@@ -25,15 +34,15 @@ export default function readAndWriteFiles(
   paths,
   templateName,
   packageName = null,
-  app = null,
+  app = null
 ) {
-  const {readDirPath, writeDirPath} = paths;
+  const { readDirPath, writeDirPath } = paths;
   return Promise.all(
-    files.map(async function(file) {
+    files.map(async file => {
       const fileData = await readFile(file, readDirPath);
       const parsedFile = parseFile(fileData, templateName, packageName, app);
-      const fileName = file.replace('Template', templateName);
+      const fileName = file.replace("Template", templateName);
       return fs.writeFile(path.join(writeDirPath, fileName), parsedFile);
-    }),
-  );
+    })
+  ).catch(e => console.error("[readAndWriteFiles] ", e));
 }
