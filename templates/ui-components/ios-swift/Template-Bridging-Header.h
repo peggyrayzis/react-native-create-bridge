fbc3c6e52f923f4f785deef312c47650ff4ee2e3
Peggy Rayzis <peggyrayzis@gmail.com>
2017-07-10 22:03:44 -0700

    fix(swift): added bridging headers

---
+++ templates/ui-components/ios-swift/Template-Bridging-Header.h
@@ -0,0 +1,20 @@
+//  Created by react-native-create-bridge
+// {{template}}-Bridging-Header.h
+
+// import RCTBridgeModule
+#if __has_include(<React/RCTBridgeModule.h>)
+#import <React/RCTBridgeModule.h>
+#elif __has_include(“RCTBridgeModule.h”)
+#import “RCTBridgeModule.h”
+#else
+#import “React/RCTBridgeModule.h” // Required when used as a Pod in a Swift project
+#endif
+
+// import RCTEventEmitter
+#if __has_include(<React/RCTViewManager.h>)
+#import <React/RCTViewManager.h>
+#elif __has_include(“RCTViewManager.h”)
+#import “RCTViewManager.h”
+#else
+#import “React/RCTViewManager.h” // Required when used as a Pod in a Swift project
+#endif
