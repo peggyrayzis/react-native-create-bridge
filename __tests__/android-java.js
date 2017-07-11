2c4080e33268f3f69e0b06b4aaef04cfe690be63
Peggy Rayzis <peggyrayzis@gmail.com>
2017-07-10 22:21:50 -0700

    fix(tests): updated snapshots, added new tests for split out templates

--- __tests__/android-java.js
+++ __tests__/android-java.js
@@ -1,39 +1,111 @@
-import path from "path";
-import { readFile, parseFile } from "../src/file-operations";
+import path from 'path';
+import { readFile, parseFile } from '../src/file-operations';
 
-describe("Android/Java", () => {
-  const templateName = "TestModule";
-  const readDirPath = path.join(__dirname, "..", "templates", "android-java");
+describe('Android/Java: UI Components', () => {
+  const templateName = 'TestModule';
+  const readDirPath = path.join(
+    __dirname,
+    '..',
+    'templates',
+    'ui-components',
+    'android-java',
+  );
 
-  it("creates a TemplateManager.java", async () => {
-    const fileData = await readFile("TemplateManager.java", readDirPath);
+  it('creates a TemplateManager.java', async () => {
+    const fileData = await readFile('TemplateManager.java', readDirPath);
     const parsedFile = parseFile(
       fileData,
       templateName,
       templateName.toLowerCase(),
-      "testapp"
+      'testapp',
     );
     expect(parsedFile).toMatchSnapshot();
   });
 
-  it("creates a TemplateModule.java", async () => {
-    const fileData = await readFile("TemplateModule.java", readDirPath);
+  it('creates a TemplatePackage.java', async () => {
+    const fileData = await readFile('TemplatePackage.java', readDirPath);
     const parsedFile = parseFile(
       fileData,
       templateName,
       templateName.toLowerCase(),
-      "testapp"
+      'testapp',
+    );
+    expect(parsedFile).toMatchSnapshot();
+  });
+});
+
+describe('Android/Java: Native Modules', () => {
+  const templateName = 'TestModule';
+  const readDirPath = path.join(
+    __dirname,
+    '..',
+    'templates',
+    'modules',
+    'android-java',
+  );
+
+  it('creates a TemplateModule.java', async () => {
+    const fileData = await readFile('TemplateModule.java', readDirPath);
+    const parsedFile = parseFile(
+      fileData,
+      templateName,
+      templateName.toLowerCase(),
+      'testapp',
     );
     expect(parsedFile).toMatchSnapshot();
   });
 
-  it("creates a TemplatePackage.java", async () => {
-    const fileData = await readFile("TemplatePackage.java", readDirPath);
+  it('creates a TemplatePackage.java', async () => {
+    const fileData = await readFile('TemplatePackage.java', readDirPath);
     const parsedFile = parseFile(
       fileData,
       templateName,
       templateName.toLowerCase(),
-      "testapp"
+      'testapp',
+    );
+    expect(parsedFile).toMatchSnapshot();
+  });
+});
+
+describe('Android/Java: Combined', () => {
+  const templateName = 'TestModule';
+  const readDirPath = path.join(
+    __dirname,
+    '..',
+    'templates',
+    'combined',
+    'android-java',
+  );
+
+  it('creates a TemplateManager.java', async () => {
+    const fileData = await readFile('TemplateManager.java', readDirPath);
+    const parsedFile = parseFile(
+      fileData,
+      templateName,
+      templateName.toLowerCase(),
+      'testapp',
+    );
+    expect(parsedFile).toMatchSnapshot();
+  });
+
+  it('creates a TemplateModule.java', async () => {
+    const fileData = await readFile('TemplateModule.java', readDirPath);
+    const parsedFile = parseFile(
+      fileData,
+      templateName,
+      templateName.toLowerCase(),
+      'testapp',
+    );
+    expect(parsedFile).toMatchSnapshot();
+  });
+
+  it('creates a TemplatePackage.java', async () => {
+    const fileData = await readFile('TemplatePackage.java', readDirPath);
+    const parsedFile = parseFile(
+      fileData,
+      templateName,
+      templateName.toLowerCase(),
+      'testapp',
     );
     expect(parsedFile).toMatchSnapshot();
   });
--- __tests__/android-kotlin.js
+++ __tests__/android-kotlin.js
@@ -1,39 +1,111 @@
-import path from "path";
-import { readFile, parseFile } from "../src/file-operations";
+import path from 'path';
+import { readFile, parseFile } from '../src/file-operations';
 
-describe("Android/Kotlin", () => {
-  const templateName = "TestModule";
-  const readDirPath = path.join(__dirname, "..", "templates", "android-kotlin");
+describe('Android/Kotlin: UI Components', () => {
+  const templateName = 'TestModule';
+  const readDirPath = path.join(
+    __dirname,
+    '..',
+    'templates',
+    'ui-components',
+    'android-kotlin',
+  );
 
-  it("creates a TemplateManager.kt", async () => {
-    const fileData = await readFile("TemplateManager.kt", readDirPath);
+  it('creates a TemplateManager.kt', async () => {
+    const fileData = await readFile('TemplateManager.kt', readDirPath);
     const parsedFile = parseFile(
       fileData,
       templateName,
       templateName.toLowerCase(),
-      "testapp"
+      'testapp',
     );
     expect(parsedFile).toMatchSnapshot();
   });
 
-  it("creates a TemplateModule.kt", async () => {
-    const fileData = await readFile("TemplateModule.kt", readDirPath);
+  it('creates a TemplatePackage.kt', async () => {
+    const fileData = await readFile('TemplatePackage.kt', readDirPath);
     const parsedFile = parseFile(
       fileData,
       templateName,
       templateName.toLowerCase(),
-      "testapp"
+      'testapp',
+    );
+    expect(parsedFile).toMatchSnapshot();
+  });
+});
+
+describe('Android/Kotlin: Native Modules', () => {
+  const templateName = 'TestModule';
+  const readDirPath = path.join(
+    __dirname,
+    '..',
+    'templates',
+    'modules',
+    'android-kotlin',
+  );
+
+  it('creates a TemplateModule.kt', async () => {
+    const fileData = await readFile('TemplateModule.kt', readDirPath);
+    const parsedFile = parseFile(
+      fileData,
+      templateName,
+      templateName.toLowerCase(),
+      'testapp',
     );
     expect(parsedFile).toMatchSnapshot();
   });
 
-  it("creates a TemplatePackage.kt", async () => {
-    const fileData = await readFile("TemplatePackage.kt", readDirPath);
+  it('creates a TemplatePackage.kt', async () => {
+    const fileData = await readFile('TemplatePackage.kt', readDirPath);
     const parsedFile = parseFile(
       fileData,
       templateName,
       templateName.toLowerCase(),
-      "testapp"
+      'testapp',
+    );
+    expect(parsedFile).toMatchSnapshot();
+  });
+});
+
+describe('Android/Kotlin: Combined', () => {
+  const templateName = 'TestModule';
+  const readDirPath = path.join(
+    __dirname,
+    '..',
+    'templates',
+    'combined',
+    'android-kotlin',
+  );
+
+  it('creates a TemplateManager.kt', async () => {
+    const fileData = await readFile('TemplateManager.kt', readDirPath);
+    const parsedFile = parseFile(
+      fileData,
+      templateName,
+      templateName.toLowerCase(),
+      'testapp',
+    );
+    expect(parsedFile).toMatchSnapshot();
+  });
+
+  it('creates a TemplateModule.kt', async () => {
+    const fileData = await readFile('TemplateModule.kt', readDirPath);
+    const parsedFile = parseFile(
+      fileData,
+      templateName,
+      templateName.toLowerCase(),
+      'testapp',
+    );
+    expect(parsedFile).toMatchSnapshot();
+  });
+
+  it('creates a TemplatePackage.kt', async () => {
+    const fileData = await readFile('TemplatePackage.kt', readDirPath);
+    const parsedFile = parseFile(
+      fileData,
+      templateName,
+      templateName.toLowerCase(),
+      'testapp',
     );
     expect(parsedFile).toMatchSnapshot();
   });
