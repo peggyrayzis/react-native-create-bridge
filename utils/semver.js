4156b77d8732225f5ddb63cf077b413cf91d28ce
Jarret Moses <jarret.moses@gmail.com>
2018-02-10 15:34:34 -0500

    chore: add missing folder from previous commit

---
+++ utils/semver.js
@@ -0,0 +1,5 @@
+const semverRegExp = /\bv?(?:0|[1-9]\d*)\.(?:0|[1-9]\d*)\.(?:0|[1-9]\d*)(?:-[\da-z-]+(?:\.[\da-z-]+)*)?(?:\+[\da-z-]+(?:\.[\da-z-]+)*)?\b/ig;
+
+module.exports = function(version) {
+  return version.match(semverRegExp)[0];
+}
